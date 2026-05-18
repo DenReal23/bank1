b4221c07e0a38487b4f6f1fa19e8375c82ef2540
DenReal23 <minakovasofya@yandex.ru>
2026-05-18 20:04:18 +0300

    Delete mavenproject1 directory

--- mavenproject1/src/main/java/denis_sofia/mavenproject1/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Account account = new Account("User", 0);
-        Avtomat avtomat = new Avtomat(account);
-        avtomat.showMenu();
-    }
-}
--- mavenproject1/src/main/java/denis_sofia/mavenproject1/enums/AccountType.java
+++
@@ -1,6 +0,0 @@
-package denis_sofia.mavenproject1.enums;
-
-public enum AccountType {
-    DEBIT,
-    CREDIT
-}
